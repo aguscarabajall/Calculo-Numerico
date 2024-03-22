af3a3b5242f85221306fc964fcfe807e4637b605
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-03-22 16:38:39 -0300

    Add files via upload

---
+++ bi_1.m
@@ -0,0 +1,35 @@
+%f(x)=x-cos(x) por biseccion
+function bi_1
+x1=0;
+x2=1;
+itmax=1000;
+ea=1e-7;
+it=0;
+estado=0;
+while estado==0
+  %ecuacion de recurrencia
+   r=(x1+x2)/2;
+  error=abs(fc(r));
+   %control de detencion
+  if error<ea
+    estado=33;
+  endif
+it=it+1;
+if it>itmax
+  estado=22;
+endif
+%actualizacion de variables
+if fc(x1)*fc(r)<0
+  x2=r;
+else
+  x1=r;
+endif
+endwhile
+disp(r)
+disp(estado)
+disp(it)
+disp(error)
+endfunction
+function y=fc(a)
+  y=a-cos(a);
+  endfunction
