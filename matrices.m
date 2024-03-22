1d91b18f391c97e07b888f1b6e02240ed9300674
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-03-22 12:44:50 -0300

    Add files via upload

---
+++ matrices.m
@@ -0,0 +1,58 @@
+function matrices
+  M=zeros(3);
+  contador=0;
+  for i=1:3
+    for j=1:3
+      contador=contador+1;
+      M(i,j)=contador;
+    endfor
+  endfor
+triangularsuperior=trs(M)
+diagonal=d(M)
+triangularinferior=tri(M)
+[maximo,posicion]=maxpo(M)
+end
+
+  function triangularsuperior=trs(A)
+    for i=1:3
+      for j=1:3
+        if i<=j
+          triangularsuperior(i,j)=A(i,j);
+        endif
+      endfor
+    endfor
+  endfunction
+
+   function triangularinferior=tri(A)
+    for i=1:3
+      for j=1:3
+        if i>=j
+          triangularinferior(i,j)=A(i,j);
+        endif
+      endfor
+    endfor
+  endfunction
+
+  function diagonal=d(A)
+    for i=1:3
+      for j=1:3
+        if i=j
+          diagonal(i,j)=A(i,j);
+        endif
+      endfor
+    endfor
+  endfunction
+
+  function [maximo,posicion]=maxpo(A)
+    maximo=A(1,1);
+    posicion=(1,1);
+    for i=1:3
+      for j=1:3
+        if maximo<A(i,j)
+          maximo=A(i,j);
+          posicion=i,j;
+        endif
+      endfor
+    endfor
+    endfunction
+
