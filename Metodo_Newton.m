f0919e8b8e7109251459ba8fba07d6eb1daa60e7
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-04-19 16:39:17 -0300

    Add files via upload

---
+++ Metodo_Newton.m
@@ -0,0 +1,10 @@
+function Metodo_Newton
+  x=0:0.1:5;
+  for i=1:length(x);
+    y(i)=MN(x(i));
+  endfor
+plot(x,y)
+endfunction
+function P=MN(h)
+  P=2*h-4*h*(h-0.5)+(8/19)*h*(h-0.5)*(h-1);
+endfunction
