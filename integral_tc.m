3a2928c0febc4af8c48b707778e6a384ac8193dd
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-04-26 15:47:40 -0300

    Add files via upload

--- integral_tc.m
+++ integral_tc.m
@@ -1,7 +1,7 @@
 function integral_tc
 vx=[-2;-1;0;1;2];
 vy=[4;1;0;1;4];
-dx=vx(2)-vx(1); %`paso
+dx=vx(2)-vx(1); %paso
 I=ITC(vy,dx)
 endfunction
 function I=ITC(vy,dx)
@@ -9,7 +9,7 @@ function I=ITC(vy,dx)
   w=ones(n,1);
   w(1)=0.5;
   w(n)=0.5;
-  I=0
+  I=0;
   for i=1:n;
     I=I+dx*vy(i)*w(i);
   endfor
---
+++ integral_tc_sc.m
@@ -0,0 +1,33 @@
+function integral_tc_sc
+vx=[-2;-1;0;1;2];
+vy=[4;1;0;1;4];
+dx=vx(2)-vx(1); %paso
+I1=ITC(vy,dx)
+I2=ISC(vy,dx)
+endfunction
+function I=ITC(vy,dx)
+  n=length(vy);
+  w=ones(n,1);
+  w(1)=0.5;
+  w(n)=0.5;
+  I=0;
+  for i=1:n;
+    I=I+dx*vy(i)*w(i);
+  endfor
+endfunction
+function I=ISC(vy,dx)
+  n=length(vy);
+  w=ones(n,1);
+  w(1)=1/3;
+  for i=2:2:n-1 %impares
+  w(i)=4/3;
+  endfor
+  for i=3:2:n-2 %pares hablamos de posicion 
+  w(i)=2/3;
+  endfor
+  w(n)=1/3;
+  I=0;
+  for i=1:n;
+   I=I+dx*vy(i)*w(i);
+  endfor
+endfunction 
