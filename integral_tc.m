350a7baaafcb33d6f0b7f874e4675441b1119482
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-04-26 15:33:50 -0300

    Add files via upload

---
+++ integral_tc.m
@@ -0,0 +1,16 @@
+function integral_tc
+vx=[-2;-1;0;1;2];
+vy=[4;1;0;1;4];
+dx=vx(2)-vx(1); %`paso
+I=ITC(vy,dx)
+endfunction
+function I=ITC(vy,dx)
+  n=length(vy);
+  w=ones(n,1);
+  w(1)=0.5;
+  w(n)=0.5;
+  I=0
+  for i=1:n;
+    I=I+dx*vy(i)*w(i);
+  endfor
+endfunction
