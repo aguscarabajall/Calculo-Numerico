4e5123b037b46b423dc9e138a191a14260f9880e
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-04-26 15:54:12 -0300

    integral_tc_sc.m

--- integral_tc_sc.m
+++ integral_tc_sc.m
@@ -5,18 +5,18 @@ dx=vx(2)-vx(1); %paso
 I1=ITC(vy,dx)
 I2=ISC(vy,dx)
 endfunction
-function I=ITC(vy,dx)
+function I=ITC(vy,paso) %no importa el nombre mientras sea consistente
   n=length(vy);
   w=ones(n,1);
   w(1)=0.5;
   w(n)=0.5;
   I=0;
   for i=1:n;
-    I=I+dx*vy(i)*w(i);
+    I=I+paso*vy(i)*w(i);
   endfor
 endfunction
-function I=ISC(vy,dx)
-  n=length(vy);
+function I=ISC(vector,paso)
+  n=length(vector);
   w=ones(n,1);
   w(1)=1/3;
   for i=2:2:n-1 %impares
@@ -28,6 +28,6 @@ function I=ISC(vy,dx)
   w(n)=1/3;
   I=0;
   for i=1:n;
-   I=I+dx*vy(i)*w(i);
+   I=I+paso*vector(i)*w(i);
   endfor
 endfunction 
