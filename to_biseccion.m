310f1a834a72a7eba25aaba301a32acf38d20517
aguscarabajall <163867814+aguscarabajall@users.noreply.github.com>
2024-07-16 19:03:56 -0300

    Add files via upload

---
+++ to_biseccion.m
@@ -0,0 +1,34 @@
+function to_biseccion
+  %intervalo [0,2];
+  a=0;
+  b=2;
+  it=0;
+  itmax=10000;
+  estado=0;
+  er=0.001;
+  while estado==0
+ %-----ecuacion de recurrencia----------------
+ r=(a+b)/2;
+ %-----control de detencion---------
+ it=it+1;
+  if itmax<it
+    estado=33;
+  endif
+  if abs(fc(r))<er
+    estado=22;
+  endif
+  %-----actualizacion de variables-------
+  if fc(a)*fc(r)<0
+    b=r;
+  else
+    a=r;
+  endif
+endwhile
+r
+estado
+it
+endfunction
+function y=fc(x)
+  y=x-2^(-x);
+endfunction
+
---
+++ to_newton.m
@@ -0,0 +1,30 @@
+function to_newton
+  r=2;
+  it=0;
+  itmax=1000;
+  er=0.001;
+  estado=0;
+  while estado==0
+    %-----recurrencia-----
+    rn=r-(fc(r)/d(r));
+    %-----detencion-----
+    it=it+1;
+    if itmax<it
+      estado=33;
+    endif
+    if abs(fc(rn))<er
+      estado=22;
+    endif
+    %-----actualizacion------
+    r=rn;
+  endwhile
+  rn
+  it
+endfunction
+function y=fc(h)
+  y=h-2^(-h);
+endfunction
+function g=d(k)
+  g=1+(2^(-k)*log(2));
+endfunction
+
